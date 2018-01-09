54a0a032a36a78866164d2e5abae8769efd1a284
Dailan Bueno <daylansantos@gmail.com>
2018-01-08 23:30:11 -0200

    Criando configuraçao por token.

---
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -0,0 +1,45 @@
+package com.example.algamoney.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig   extends AuthorizationServerConfigurerAdapter {
+
+	
+	@Autowired 
+	private AuthenticationManager authenticationManager; 
+
+	@Override 
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception { 
+ 		clients.inMemory()  // ficar em memoria
+ 			.withClient("angular")  // qual nome do cliente
+ 			.secret("@ngul@r0")  // usuário
+			.scopes("read", "write") 
+ 			.authorizedGrantTypes("password")  
+			.accessTokenValiditySeconds(1800); //quanto tempo esse token vai ficar funcionando.
+ 	} 
+ 	 
+ 	@Override 
+ 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception { 
+ 		endpoints 
+ 			.tokenStore(tokenStore()) // onde o token fica armazenado 
+ 			.authenticationManager(authenticationManager); 
+ 	} 
+ 	 
+ 	@Bean 
+ 	public TokenStore tokenStore() { 
+ 		return new InMemoryTokenStore(); 
+ 	} 
+
+
+}
--- src/main/java/com/example/algamoney/api/config/ResourceServeConfig.java
+++ src/main/java/com/example/algamoney/api/config/ResourceServeConfig.java
@@ -4,19 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer; 
 
 @Configuration //opecional já que @EnableWebSecurity já implementa essa anotation
 @EnableWebSecurity
-public class SecurityConfig  extends WebSecurityConfigurerAdapter{
+public class ResourceServeConfig  extends ResourceServerConfigurerAdapter {
 	
 	
 	/**
 	 * Responsavável  autenticar o usuário
 	 */
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 		.withUser("admin").password("admin").roles("ROLE"); // ROLES É PERMISÕES.
 		
@@ -27,16 +28,25 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	 * Responsável por autorização das requisições
 	 */
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 	
 		http.authorizeRequests()
 		.antMatchers("/categoria/listar").permitAll() // permitir acesso publico
 		.anyRequest().authenticated() // Permitir acesso somente por usuário a todas as outras requisições
 		.and()
-		.httpBasic() // modo basico de requisição
-		.and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // não será criado sessão para o servidor.
+	//	.httpBasic() // modo basico de requisição
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // não será criado sessão para o servidor.//
 		.and()
 		.csrf().disable();
+	}
+	
+	/*
+	 * Deixar servidor stateless
+	 * (non-Javadoc)
+	 * @see org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter#configure(org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer)
+	 */
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
 	}
 }
