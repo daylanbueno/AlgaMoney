0d3a073cd5ac1d0ce78160a8c622e855661347aa
Dailan Bueno <daylansantos@gmail.com>
2018-01-14 14:52:11 -0200

    Configurando filtro cors.

---
+++ src/main/java/com/example/algamoney/api/cors/CorsFilter.java
@@ -0,0 +1,62 @@
+package com.example.algamoney.api.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE) // Para ter preferencia nas requisições
+public class CorsFilter implements Filter {
+	
+	private String origenPermitida = "http://localhost:8000"; // deverá ser configurado para ambientes.
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		resp.setHeader("Access-Control-Allow-Origin", origenPermitida); 
+		resp.setHeader("Access-Control-Allow-Credentials", "true"); 
+		
+		if("OPTIONS".equals(req.getMethod()) && origenPermitida.equals(req.getHeader("Origin"))) {
+			
+			resp.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS"); 
+			resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept"); 
+			resp.setHeader("Access-Control-Max-Age", "3600"); 
+
+		}else {
+			chain.doFilter(request,response);
+		}
+	}
+	
+	/*
+	 * Implementação obrigatória devido a interface Filter.
+	 */
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/*
+	 * Implementação obrigatória devido a interface Filter.
+	 */
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
